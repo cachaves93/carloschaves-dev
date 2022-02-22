a215b2962f77f0b1bb77dd56e9d6dd7acb11d5f3
Carlos Chaves <carloschaves@ACTs-MacBook-Pro.local>
2022-02-22 01:21:38 -0300

    -refactoring home page -minor styling adjustments

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,25 +9,15 @@ import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor(
-    @Inject(PLATFORM_ID) private platformId: any,
+    @Inject(PLATFORM_ID) private platformId: Object,
   ) { }
 
   ngOnInit(): void {
-    // if (isPlatformBrowser(this.platformId)) this.loadLinkedInPlatformScript();
-  }
-
-  private loadLinkedInPlatformScript(): void {
-    const html = document.getElementsByTagName('html')[0] as HTMLElement;
-    const script = document.createElement('script') as HTMLScriptElement;
-    script.type = 'text/javascript';
-    script.src = 'https://platform.linkedin.com/badges/js/profile.js';
-    script.onerror = this.scriptCallback
-    script.onload = this.scriptCallback
-    html.append(script);
-  }
-
-  private scriptCallback(): void {
-    console.log('script error')
+    if (isPlatformBrowser(this.platformId)) {
+      window.addEventListener('message', (message: MessageEvent) => {
+        console.log(message);
+      })
+    }
   }
 
 }
