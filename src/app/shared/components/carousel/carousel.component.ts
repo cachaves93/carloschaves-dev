cd90019a4ccdfeb6e9a192d5e2432a0a3dcf2dbf
Carlos Chaves <carloschaves@ACTs-MacBook-Pro.local>
2022-02-23 01:58:32 -0300

    removing more assets

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -9,11 +9,20 @@ export class CarouselComponent {
 
   @Input() imgUrls: string[] = [];
 
+  public isLoadingImg: boolean = true;
+  public canTriggerLoading: boolean = true;
+
   currentImgIndex: number = 0
 
   constructor() { }
 
   updateImgIndexValueByNumber(number: number) {
     this.currentImgIndex += number;
+    setTimeout(() => { if (this.canTriggerLoading) this.isLoadingImg = true }, 200);
+  }
+
+  toggleLoading(bool: boolean): void {
+    this.canTriggerLoading = bool;
+    this.isLoadingImg = bool;
   }
 }
